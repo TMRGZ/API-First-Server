2b4a346e4f31b5027662373732741542ce95a93a
TMRGZ <miguelrg5605@outlook.es>
2023-07-16 15:11:22 +0200

    First commit

---
+++ src/test/java/com/personal/api_first_server/ApiFirstServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.api_first_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFirstServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
